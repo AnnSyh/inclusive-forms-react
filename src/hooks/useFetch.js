fae28db8e303c22bbc867b4550c06679c6b417e7
Даниил <khromov865@mail.ru>
2025-10-01 11:50:02 +0300

    feat: added api client

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const useFetch = (url) => {
+  const [status, setStatus] = useState({
+    isLoading: false,
+    data: null,
+    error: null,
+  });
+
+  const getFetch = async () => {
+    setStatus({ data: null, error: null, isLoading: true });
+
+    try {
+      setStatus((prev) => ({ ...prev, isLoading: true }));
+      const response = await fetch(`${BASE_URL}${url}`);
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP, статус: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setStatus({
+        data,
+        error: null,
+        isLoading: false,
+      });
+    } catch (error) {
+      setStatus({
+        data: null,
+        error: `Ошибка при запросе: ${error}`,
+        isLoading: false,
+      });
+    }
+  };
+  useEffect(() => {
+    getFetch(url);
+  }, [url]);
+  return status;
+};
