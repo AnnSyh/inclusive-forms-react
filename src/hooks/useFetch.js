1a80fe40ab58231621e9dd0121dfab963f405fdd
Даниил <khromov865@mail.ru>
2025-10-02 20:01:06 +0300

    feat: added link

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL | 'http://89.169.154.49:8000';
 
 export const useFetch = (url) => {
   const [status, setStatus] = useState({
