09c84645aaa99d0056045a045570e6372ae446f1
Даниил <khromov865@mail.ru>
2025-10-02 20:11:05 +0300

    fix: url

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL | 'http://89.169.154.49:8000';
+const BASE_URL = 'http://89.169.154.49:8000';
 
 export const postQuestions = async (body) => {
   const response = await fetch(`${BASE_URL}/api/form/1/submit/`, {
