2af841b0b6b3f6c920a7d9ef1b81f9255919aeaf
Даниил <khromov865@mail.ru>
2025-10-02 20:29:32 +0300

    removed proxy

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react";
 
+const BASE_URL = 'http://89.169.154.49:8000';
+
 export const useFetch = (url) => {
-  const [status, setStatus] = useState({ isLoading: false, data: null, error: null });
+  const [status, setStatus] = useState({
+    isLoading: false,
+    data: null,
+    error: null,
+  });
 
   const getFetch = async () => {
     setStatus({ data: null, error: null, isLoading: true });
+
     try {
-      const response = await fetch(`/api/proxy${url}`, {
-        method: 'GET',
-        headers: { 'Accept': 'application/json' },
-      });
-      if (!response.ok) throw new Error(`HTTP ${response.status}`);
+      setStatus((prev) => ({ ...prev, isLoading: true }));
+      const response = await fetch(`${BASE_URL}${url}`);
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP, статус: ${response.status}`);
+      }
+
       const data = await response.json();
-      setStatus({ data, error: null, isLoading: false });
+      setStatus({
+        data,
+        error: null,
+        isLoading: false,
+      });
     } catch (error) {
-      setStatus({ data: null, error: `Ошибка при запросе: ${error}`, isLoading: false });
+      setStatus({
+        data: null,
+        error: `Ошибка при запросе: ${error}`,
+        isLoading: false,
+      });
     }
   };
-
-  useEffect(() => { getFetch(); }, [url]);
+  useEffect(() => {
+    getFetch(url);
+  }, [url]);
   return status;
 };
